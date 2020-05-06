640140fb90f26a27ae1754a1dd82f3c6c5063dc5
sigmar <sigmar@wuxinextcode.com>
2020-05-06 11:26:16 +0000

    erm

--- formula.rb
+++ formula.rb
@@ -1,6 +1,6 @@
-class Foo < Formula
+class Gorpipe < Formula
     desc ""
     homepage "https://github.com/gorpipe/gor"
     url "https://sigmarkarl.github.io/gorpipe"
     #sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
-}
+end
