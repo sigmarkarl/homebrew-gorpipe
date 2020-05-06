54e40a618e0cd3202aab6b6839369c7eae517162
sigmar <sigmar@wuxinextcode.com>
2020-05-06 12:43:42 +0000

    d

--- gor.rb
+++ gor.rb
@@ -4,4 +4,8 @@ class Gor < Formula
     homepage "https://github.com/gorpipe/gor"
     url "https://sigmarkarl.github.io/gorpipe.gz"
     sha256 "b283b634d4add35c8fdab8248bf6b73c3443faf63d13f0aead2ccd46a5e5f090"
+
+    def install
+        system "echo" "'hoho'"
+    end
 end
