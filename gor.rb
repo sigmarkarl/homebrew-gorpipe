5c15329a1035fbf926affff9cb01268639d276ee
sigmar <sigmar@wuxinextcode.com>
2020-05-06 12:10:03 +0000

    e

--- gor.rb
+++ gor.rb
@@ -1,5 +1,6 @@
 class Gor < Formula
     desc ""
+    version "2.4.1"
     homepage "https://github.com/gorpipe/gor"
     url "https://sigmarkarl.github.io/gorpipe.gz"
     sha256 "b283b634d4add35c8fdab8248bf6b73c3443faf63d13f0aead2ccd46a5e5f090"
