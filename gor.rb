cb1fbd1458578aae281defeab04fe50326403dd3
sigmar <sigmar@wuxinextcode.com>
2020-05-06 12:44:52 +0000

    d

--- gor.rb
+++ gor.rb
@@ -6,6 +6,6 @@ class Gor < Formula
     sha256 "b283b634d4add35c8fdab8248bf6b73c3443faf63d13f0aead2ccd46a5e5f090"
 
     def install
-        system "echo" "'hoho'"
+        system "echo", "'hoho'"
     end
 end
