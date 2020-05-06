7e67043a30460daf2974fb0abd6847a6ee0d79bf
sigmar <sigmar@wuxinextcode.com>
2020-05-06 11:17:11 +0000

    add gorpipe class

---
+++ formula.rb
@@ -0,0 +1,6 @@
+class Foo < Formula
+    desc ""
+    homepage "https://github.com/gorpipe/gor"
+    url "https://sigmarkarl.github.io/gorpipe"
+    #sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
+}
